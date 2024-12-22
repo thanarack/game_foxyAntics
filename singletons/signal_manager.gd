324d9b4fa4556e6ca5ec61f8c69bce15ab5fa1d1
Thanarak <thanarackc@gmail.com>
2024-12-23 00:10:09 +0700

    add: ep 400 snail movement

---
+++ singletons/signal_manager.gd
@@ -0,0 +1,3 @@
+extends Node
+
+signal on_enemy_hit(point: int, enemy_position: Vector2)
--- singletons/sound_manager.gd
+++ singletons/sound_manager.gd
@@ -35,5 +35,6 @@ var SOUNDS = {
 func play_clip(player: AudioStreamPlayer2D, clip_key: String):
 	if SOUNDS.has(clip_key) == false:
 		return
+
 	player.stream = SOUNDS[clip_key]
 	player.play()
