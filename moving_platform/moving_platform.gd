019cf6139c8768ecc5c294e31ddda86b98e145bd
Thanarak <thanarackc@gmail.com>
2025-03-01 14:21:52 +0700

    add: 428 spinning spikes

---
+++ moving_platform/moving_platform.gd
@@ -0,0 +1,31 @@
+extends AnimatableBody2D
+
+@export var p1: Marker2D
+@export var p2: Marker2D
+@export var speed: float = 60
+
+var _time_to_move: float = 0.0
+var _tween: Tween
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_time_to_move()
+	set_moving()
+	print("MP Ready")
+
+
+func _exit_tree() -> void:
+	_tween.kill()
+
+
+func set_time_to_move() -> void:
+	var delta = p1.global_position.distance_to(p2.global_position)
+	_time_to_move = delta / speed
+
+
+func set_moving() -> void:
+	_tween = get_tree().create_tween()
+	_tween.set_loops(0)
+	_tween.tween_property(self, "global_position", p1.global_position, _time_to_move)
+	_tween.tween_property(self, "global_position", p2.global_position, _time_to_move)
